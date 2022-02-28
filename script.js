04a0c8eed316c4588129cd3f41a71e64f00abded
Mateus Mauricio Toledo <mateusmtoledo1@gmail.com>
2022-02-27 23:45:23 -0300

    Add event listeners to the numbers

--- script.js
+++ script.js
@@ -18,6 +18,19 @@ function operate(a, b, operator) {
     return operator(a, b);
 }
 
+function updateDisplay(num) {
+    display.textContent = num;
+}
+
+let num1 = 0;
+let num2 = 0;
+const display = document.querySelector('.display');
 const numbers = [...document.querySelectorAll('.number')];
 const numberKeys = [];
-numbers.forEach(number => numberKeys[number.textContent] = number);
+numbers.forEach(number => {
+    numberKeys[number.textContent] = number;
+    number.addEventListener('click', () => {
+        num1 = Number(number.textContent);
+        updateDisplay(num1);
+    });
+});
