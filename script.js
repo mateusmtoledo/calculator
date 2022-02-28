a4821eecaebdfc55ee9ceb649760be0ad836788e
Mateus Mauricio Toledo <mateusmtoledo1@gmail.com>
2022-02-27 22:56:45 -0300

    Create initial layout

---
+++ script.js
@@ -0,0 +1,13 @@
+// const number = [];
+// const row = [];
+// const numberContainer = document.querySelector('.number-container');
+
+// for (let i = 0; i < 3; i++) {
+//     row[i] = document.createElement('div');
+//     numberContainer.appendChild(row[i]);
+//     for (let j = 9 - 3 * i; j > (2 - i) * 3; j--) {
+//         number[j] = document.createElement('div');
+//         number[j].textContent = j;
+//         numberContainer.appendChild(number[j]);
+//     }
+// }
